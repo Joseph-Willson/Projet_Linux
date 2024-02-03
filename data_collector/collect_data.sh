076554b18e1ebeac3651b22824e739d2fd196bd3
jwillson <j.willson@outlook.fr>
2024-02-03 22:59:51 +0100

    foor

--- data_collector/collect_data.sh
+++ data_collector/collect_data.sh
@@ -1,1 +1,3 @@
-curl https://data.wa.gov/api/views/f6w7-q2d2/rows.csv?accessType=DOWNLOAD
+#!/bin/bash
+
+curl -o "C:\Users\JWill\OneDrive\Bureau\MOSEF\Linux\data.csv" "https://data.wa.gov/api/views/f6w7-q2d2/rows.csv?accessType=DOWNLOAD"
